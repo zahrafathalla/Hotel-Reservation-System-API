8bd164bccc4e9e95641bd42b37f5351e0d72d741
ismaeelabulmaaty <ismaeelmatty@gmail.com>
2024-08-21 01:42:56 +0300

    test

--- HotelReservationSystem.Mediator/obj/Debug/net8.0/HotelReservationSystem.Mediator.AssemblyInfo.cs
+++ HotelReservationSystem.Mediator/obj/Debug/net8.0/HotelReservationSystem.Mediator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelReservationSystem.Mediator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e46b72e6706b1326ea2b97bcc18a32e012cb102")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelReservationSystem.Mediator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelReservationSystem.Mediator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HotelReservationSystem.Service/obj/Debug/net8.0/HotelReservationSystem.Service.AssemblyInfo.cs
+++ HotelReservationSystem.Service/obj/Debug/net8.0/HotelReservationSystem.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelReservationSystem.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e46b72e6706b1326ea2b97bcc18a32e012cb102")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelReservationSystem.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelReservationSystem.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
